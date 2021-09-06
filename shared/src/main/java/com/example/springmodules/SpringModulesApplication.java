d87ba3d5344909f4c753dbc48c4915d95057d9bb
lgonzalezesp <N2uCvXWuzkNZNW2mwnG7>
2021-09-06 18:41:09 -0300

    init

---
+++ shared/src/main/java/com/example/springmodules/SpringModulesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModulesApplication.class, args);
+    }
+
+}
